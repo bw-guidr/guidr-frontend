868295c6f98425cde11fb3d89eac404440acf26d
Cesar Aponte <cesar.aponte.1996.vazquez@gmail.com>
2019-07-29 13:30:23 -0400

    Action and Reducer for login and signup setup. Waiting on back end to get routes setup correctly. Router setup, simple navbar made just to test routing, no styling or interfaces made yet.

---
+++ src/Store/Reducer/Reducer.js
@@ -0,0 +1,49 @@
+import {
+    LOGIN_START,
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE,
+    ERROR_MESSAGE,
+    GET_USERS,
+    LOADING
+} from '../Action/Action'
+
+const initialState = {
+    isLoggingIn:false,
+    isSigningUp:false,
+    error:'',
+    isFetching:false,
+    dropped: false,
+    tripData: [],
+    trips: []
+}
+
+export const reducer = (state=initialState,action) => {
+    switch(action.type){
+        case LOGIN_START:
+            return {
+                ...state,
+                isLoggingIn:true,
+                error:''
+            };
+        case LOGIN_SUCCESS:
+            return {
+                ...state,
+                isLoggingIn:false,
+                error:''
+            };
+        case LOGIN_FAILURE:
+            return {
+                ...state,
+                error:action.payload,
+                isLoggingIn:false
+            };
+        case GET_USERS:
+            return Object.assign({}, state, {users: action.users, loading: false, error: ''})
+        case ERROR_MESSAGE:
+            return Object.assign({}, state, {error: action.errorMessage, loading: false})
+        case LOADING:
+            return Object.assign({}, state, {loading: true})
+        default:
+            return state;    
+        }
+    }
