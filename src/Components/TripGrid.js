c9f77a76c7a794459e3475f508a56d07c4e5b3d2
maddiestrange <maddie.strange@gmail.com>
2019-07-29 13:25:19 -0500

    added prelim Trip card+grid, renamed Dashboard to Portfolio

---
+++ src/Components/TripGrid.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import TripCard from './TripCard'
+
+export default function TripGrid() {
+  
+  const [trips, setTrips] = useState([]);
+  useEffect(() => {
+
+      axios
+        .get('')
+        .then(response => {
+          setCharacters(response.data.results);
+          console.log('character API:', response.data.results)
+          console.log('characters:', characters)
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+  }, [])
+
+  return (
+  <section className='trip-grid'>
+    {trips.map(trips =>{
+            return <TripCard name={trips.name} 
+                               description={trips.description}
+                               episodes={trips.miles}
+                               img={trips.image}/> })}
+  </section>
+  )
+}
