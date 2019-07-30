5dc4af94b8700952a5c7ce6830cd6b9ef6c6d1a6
Chris York <christopherayork1992@gmail.com>
2019-07-30 15:28:14 -0400

    Home Page listing out professional trips

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,35 @@
 import React from 'react'
 import Header from './Header';
 import Body from './Layout';
+import axios from 'axios';
+import TripCard from './TripCard';
 
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      trips: [],
+      success: 0
+    };
+    this.getAllTrips();
+  }
+
+  getAllTrips = async () => {
+    let res = await axios.get('https://guidr-backend-justin-chen.herokuapp.com/trips/all');
+    console.log(res.data);
+    let prof = res.data.filter(c => c.trip_type === 'Professional');
+    console.log(prof);
+    this.setState({trips: prof, success: 1});
+  };
+
   render(){
     return(
         <Body>
           <Header />
           <main className='home-content'>
             <article>
-              {/* Log in, go to dashboard */}
+              { this.state.success ? this.state.trips.map(o => <TripCard {...o} key={o.id} />) : null }
               Placeholder text
             </article>
           </main>
