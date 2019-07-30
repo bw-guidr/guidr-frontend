b321d2d5d44412ee7a997b5fafb0252cdcdb7403
Neha Patel <neha.r.p@gmail.com>
2019-07-30 12:39:36 -0700

    ProfileForm.js added useState for userProfile

--- src/Components/ProfileForm.js
+++ src/Components/ProfileForm.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 function ProfileForm () {
-
+    const [userProfile, setUserProfile] = useState({
+        title: '',
+        tagline: '',
+        age: '',
+        guideXP: '';
+    });
     return(
         <div>
-            
+
         </div>
     )
 }
