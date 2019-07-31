43174be834f5c8847f76c27674c1f71226edec7a
Neha Patel <neha.r.p@gmail.com>
2019-07-31 15:21:07 -0700

    Footer.js styling Grid elements

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,39 +1,56 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Grid } from 'semantic-ui-react';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Grid, GridColumn } from "semantic-ui-react";
 
 function Footer() {
-
-    return (
-        <Grid>
-            <Grid.Row className="footer-nav">
-                <NavLink>Home</NavLink>
-                <NavLink>Register</NavLink>
-                <NavLink>Login</NavLink>
-                <NavLink>Blog</NavLink>
-                <NavLink>News</NavLink>
-                <NavLink>Contact</NavLink>
-            </Grid.Row>
-            <Grid.Row className="footer-contact-info">
-                <div className="footer-contact-us">
-                    <h3>Contact Us</h3>
-                    <p className="footer-contact-email">hello@email.com</p>
-                </div>
-                <div className="footer-location">
-                    <h3>Location</h3>
-                    <address>2019 Lambda Lane <br />San Francisco, CA<br />United States</address>
-                </div>
-                <div className="footer-business-name">
-                    <h3>Guidr</h3>
-                    <p>Build your outdoor resume.</p>
-                </div>
-            </Grid.Row>
-            <Grid.Row className="footer-copyright">
-                Copyright &copy; Guidr
-                <p>All rights reserved</p>
-            </Grid.Row>
-        </Grid>
-    )
+  return (
+    <Grid columns={6}>
+      <Grid.Row textAlign="right">
+        <Grid.Column>
+          <NavLink>Home</NavLink>
+        </Grid.Column>
+        <Grid.Column>
+          <NavLink>Register</NavLink>
+        </Grid.Column>
+        <Grid.Column>
+          <NavLink>Login</NavLink>
+        </Grid.Column>
+        <Grid.Column>
+          <NavLink>Blog</NavLink>
+        </Grid.Column>
+        <Grid.Column>
+          <NavLink>News</NavLink>
+        </Grid.Column>
+        <Grid.Column>
+          <NavLink>Contact</NavLink>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column>
+          <h3>Contact Us</h3>
+          <p className="footer-contact-email">hello@email.com</p>
+        </Grid.Column>
+        <Grid.Column>
+          <h3>Location</h3>
+          <address>
+            2019 Lambda Lane <br />
+            San Francisco, CA
+            <br />
+            United States
+          </address>
+        </Grid.Column>
+        <Grid.Column>
+          <h3>Guidr</h3>
+          <p>Build your outdoor resume.</p>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        Copyright &copy; Guidr
+        <br />
+        All rights reserved
+      </Grid.Row>
+    </Grid>
+  );
 }
 
-export default Footer
+export default Footer;
