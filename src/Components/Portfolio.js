26bbe694bb77ab111950cfd4b79b949e1d766574
Neha Patel <neha.r.p@gmail.com>
2019-08-01 10:21:01 -0700

    Added footer to all pages

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,25 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import Header from './Header';
-import Body from './Layout';
-import PortfolioGrid from './PortfolioGrid'
-import TripForm from './TripForm';
-import Profile from './Profile';
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import Body from "./Layout";
+import PortfolioGrid from "./PortfolioGrid";
+import TripForm from "./TripForm";
+import Profile from "./Profile";
+import Footer from "./Footer";
+import styled from "styled-components";
 
+export default function Portfolio(props) {
+  const [update, setUpdate] = useState();
 
-export default function Portfolio(props){
-    const [update, setUpdate] = useState();
+  return (
+    <Body>
+      <Header />
+      <main className="home-content">
+        <Profile {...props} />
+        <h2>Tours</h2>
 
-    return(
-        <Body>
-          <Header />
-          <main className='home-content'>
-              <Profile {...props} />
-              <h2>Tours</h2>
-
-              <PortfolioGrid update={update} />
-              <TripForm setUpdate={setUpdate}/>
-          </main>
-        </Body>
-    )
+        <PortfolioGrid update={update} />
+        <TripForm setUpdate={setUpdate} />
+      </main>
+      <Footer />
+    </Body>
+  );
 }
