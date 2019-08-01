d29f02f3141c88d50b6d75ccb1c03ed40e418b4c
maddiestrange <maddie.strange@gmail.com>
2019-08-01 14:39:55 -0500

    working on portfoliocard styling

--- src/Components/PortfolioCard.js
+++ src/Components/PortfolioCard.js
@@ -1,6 +1,77 @@
 import React, {useState,useEffect} from 'react'
-import { Card, Icon, Image, Button, Form } from 'semantic-ui-react'
+import { Card, Button, Checkbox } from 'semantic-ui-react'
+import styled from 'styled-components'
 import placeholderImg from '../Assets/Trips/eheE9Nxg.png'
+import './Fonts.css';
+import Palette from './Palette';
+
+
+const Container = styled.div`
+  width: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 50px 0;
+`;
+
+const FormStyled = styled.form`
+
+  font-family: 'Montserrat', serif;
+  width: 50%;
+  
+  fieldset {
+    border: 0;
+    display: flex;
+    flex-wrap: wrap;
+    
+    h2 {
+      font-family: 'Montserrat', serif;
+      width: 100%;
+      text-align: center;
+    }
+    input {
+      font-family: 'Montserrat', serif;
+      width: 100%;
+      margin: 5px 0;
+      border-radius: 10px;
+      height: 30px;
+      border: 1px solid #424242;
+      padding: 5px;
+    }
+    input#ct-title, input#ct-loc {
+      width: 40%;
+    }
+    input#ct-desc {
+      height: 100px;
+    }
+    #btn-container {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      margin-top: 30px;
+      
+      button#ct-submit {
+        width: 120px;
+        height: 40px;
+        font-size: 1.7rem;
+        background: ${Palette.secondary.highlight};
+        border: 0;
+        border-radius: 3px;
+      }
+    }
+    div#ct-type-container {
+      display: flex;
+      justify-content: space-evenly;
+      padding-top: 60px;
+      
+      span {
+        font-size: 1.3rem;
+      }
+    }
+  }
+  
+`;
 
 export default function PortfolioCard (props) {
   const [thisTrip, setThisTrip] = useState({description:'',title:'',miles:''});
@@ -12,30 +83,27 @@ export default function PortfolioCard (props) {
   console.log('Props:', props)
   console.log('thisTrip:', thisTrip)
   return (
-    <Card className='pTripCard'>
+    <Container>
       {props.editNum === props.id ? (
       <>
-        <Card.Content>
-          <Form>
-            <Form.Input label='Title' type='text' 
-            name='title' value={thisTrip.title} 
-            onChange={(e) => editHandler(e)} size='mini'/>
-
-            <Form.Input label='Description' type='text' 
-            name='description' value={thisTrip.description} 
-            onChange={(e) => editHandler(e)} size='mini'/>
-
-            <Form.Input label='Miles' type='text' 
-            name='miles' value={thisTrip.miles} 
-            onChange={(e) => editHandler(e)} size='mini'/>
-          </Form>
-        </Card.Content>
-        <Card.Content>
-          <Button.Group compact widths={2}>
-              <Button color='yellow' onClick={() => props.toggleEdit(props.id)}>Cancel</Button>
-              <Button color='green' onClick={(e) => props.editTrip(e, thisTrip)}>Submit</Button>
-          </Button.Group> 
-        </Card.Content>
+        <FormStyled id='trip-form'>
+          <fieldset>
+          <h2>Edit Trip</h2>
+          <input id='ct-title' label='Title' type='text' 
+          name='title' value={thisTrip.title} 
+          onChange={(e) => editHandler(e)} size='mini'/>
+          <input id='ct-desc' label='Description' type='text' 
+          name='description' value={thisTrip.description} 
+          onChange={(e) => editHandler(e)} size='mini'/>
+          <input id='ct-mt' label='Miles' type='text' 
+          name='miles' value={thisTrip.miles} 
+          onChange={(e) => editHandler(e)} size='mini'/>
+          <div id='btn-container'>
+              <button id='ct-submit' color='yellow' onClick={() => props.toggleEdit(props.id)}>Cancel</button>
+              <button id='ct-submit' onClick={(e) => props.editTrip(e, thisTrip)}>Submit</button>
+          </div> 
+        </fieldset>
+        </FormStyled>
       </>
       ) : (
       <>
@@ -55,11 +123,20 @@ export default function PortfolioCard (props) {
         <div className='ui green button' color='green' onClick={(e) => props.toggleEdit(props.id)}>Edit</div>
         <div className='ui red button'  onClick={(e) => props.removeTrip(e, props.id)}>Remove</div>
         </Button.Group> 
-        <Button color='blue' className="proButton"  onClick={() => props.togglePro(props.id)}>Pro</Button>
+        <div id='ct-type-container'>
+              <span>Professional</span>
+              <Checkbox toggle name='trip_type' />
+              <span>Private</span>
+            </div>
       </Card.Content>
       </>
     )}
-    </Card>
+     <div id='ct-type-container'>
+              <span>Professional</span>
+              <Checkbox toggle name='trip_type' />
+              <span>Private</span>
+            </div>
+    </Container>
   );
   
   function editHandler(e) {
